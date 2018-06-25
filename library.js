fc7234734ca2dbe636fee4bad4901c09ffb9e335
Megan M <mmacdonald@techtonicgroup.com>
2018-06-25 13:36:34 -0600

    initial commit

---
+++ library.js
@@ -0,0 +1,20 @@
+var Library = function(){
+
+  this._bookShelf = new Array();
+};
+
+
+Library.prototype.addBook = function (book) {
+
+};
+
+Library.prototype.removeBookbyTitle = function (title) {
+
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  window.gLibrary = new Library();
+  window.gLibraryTwo = new Library();
+});
