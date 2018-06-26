7d1580532dbee0792f9c72838b232aff0599a5aa
Megan M <mmacdonald@techtonicgroup.com>
2018-06-26 12:10:59 -0600

    added Book objects for testing

--- js/library.js
+++ js/library.js
@@ -116,14 +116,19 @@ var book7 = new Book("Eragon", "Christopher Paolini", 662, "August 2003");
 var book8 = new Book("The Martian", "Andy Wier", 207, "February 2011");
 var book9 = new Book("Harry Potter and the Order of the Phoenix", "JK Rowling", 700, "June 2003");
 var book10 = new Book("The Wise Man's Fear", "Patrick Rothfuss", 540, "March 2011");
+var book11 = new Book("Cloud Atlas", "David Mitchell", 250, "October 2012");
+var book12 = new Book("The Cloud Atlas", "Liam Callanan", 190, "October 2004");
 var fiveBooks = [book6, book7, book8, book9, book10];
+var firstfiveBooks = [book1, book2, book3, book4, book5];
+var tricksyBooks = [book11, book12];
 
 
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  window.gLibrary = new Library();
-  gLibrary.addBooks(fiveBooks);
-
+  window.goldenLibrary = new Library();
+  goldenLibrary.addBooks(firstfiveBooks);
+  goldenLibrary.addBooks(fiveBooks);
+  goldenLibrary.addBooks(tricksyBooks);
 });
